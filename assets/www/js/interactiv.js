9147345c39d6f1f46e50ee0a4e26966b3546a075
adn <eapv_25@hotmail.com>
2013-10-30 05:51:27 -0500

    structure change, login and filters

--- assets/www/js/interactiv.js
+++ assets/www/js/interactiv.js
@@ -1,22 +1,79 @@
-$(document).ready(function(){
+//$(document).ready(function(){
+$(document).on("pagebeforeshow", function(e){//pageinit
 //pregunta : interativ/question
 
 
-$('#pregu').click(function() {
-  	alert('hello');
+
+      // $(".goin").click(function(event)
+      //   {
+      //     event.preventDefault();
+      //     preguntas();
+      //     return false;
+      //   });
+//CARGADO DE PREGUNTAS MULTIPLE OPCION!!!!!!!
+$(document).on( "click","#goin",function(event)
+//$("#goin").click(function(event)
+{
+  //alert('he');
+  //event.preventDefault();
+  asignar_place('preguntas',this);
+  //obtener el place anterior!!!!! y colocarlo como link bajo ayuda!!!!!!!
+  preguntas();
+  return false;
+});
+
+
+
+$(document).on("click","#particip",function(e){
+ 
+           var auth = 0;
+          var atiu= $(this).attr('datum');
+          //alert(atiu);
+          if ( atiu == 'yes'){
+            auth = 1;
+            //alert('atiu');
+          }
+
+          if (auth == 0){
+              authentic(); 
+              location.hash = "#auth"
+          }else{
+              particip(); 
+              location.hash = "#particip"
+          }
+          
+          return false;
+});
+
+
+$(document).on("click","#pregunt",function(e){
+ 
+        pregunt();
+        return false;
+});
+
+
+
+
+
+
+//PREGUNTAR!!!!!!!
+//$('#pregu').click(function() {
+$('article').on("click","#pregu",function(e){
+  	/*alert('hello');*/
 	//alert($('input[name=radio-choice]:checked').val());
 	var ptitulo = $('input[name=pname]');
 	var pdetalle = $('textarea[name=ptextarea]');
 	var tema=$('#form_pregu').attr('class');
-
+	alert('se esta enviando su pregunta, espere');
 	if ($('input[name=pname]').not(':empty')){
   			var ptitu = ptitulo.val();
   			var pdetall = pdetalle.val();
   			var iduser= 7;
-  			alert('titulo:'+ptitu);
-  			alert('detalle:'+pdetall);
-  			alert('tema:'+tema);
-  			alert('finmensaje')
+  			//alert('titulo:'+ptitu);
+  			//alert('detalle:'+pdetall);
+  			//alert('tema:'+tema);
+  			//alert('finmensaje')
   			$.ajax({
 	            type: "GET",
 	            dataType: "json",
@@ -28,6 +85,7 @@ $('#pregu').click(function() {
 	        			'idtema': tema},
 	            success: function(response){
 	                alert('thanks');
+	                particip();
 
 	            },
 	            error: function (xhr, ajaxOptions, thrownError) {
@@ -47,45 +105,7 @@ $('#pregu').click(function() {
 
 
 
-// $('#resolv').click(function(e){
-//   	alert('hello');
-// 	//alert($('input[name=radio-choice]:checked').val());
-// 	var quest=$('fieldset').attr('id');
-// 	if ($('input[name=radio-choice]').is(':checked')){
-//   			var rad_val = $('input:radio[name=radio-choice]:checked').val();
-
-//   			alert(quest);
-//   			alert(rad_val);
-//   			$.ajax({
-// 	            type: "GET",
-// 	            dataType: "json",
-// 	            url: "http://localhost:8000/interactiv/resolv",
-// 	            data: { 'send_resul': rad_val,
-// 	        			'quest':quest},
-// 	            success: function(response){
-// 	                alert('thanks');
-
-// 	            },
-// 	            error: function (xhr, ajaxOptions, thrownError) {
-// 			        alert(xhr.responseText);
-// 			        alert(thrownError);
-// 			        alert('error');
-// 			    }
-// 	        });
-// 	        //window.location.replace("evento1-interactiv-t1.html");
-//   	}else{
-//   		alert('not checked');
-//   		var radio_value=0;
-//   	}
-
-
-// });
-/*
-$('#resolv').live("click",function(e){
-  	alert('hello');
-});
-*/
-
+//RESOLVER!!!!!!!
 $('article').on("click","#resolv",function(e){
   	alert('hello');
 	//alert($('input[name=radio-choice]:checked').val());
@@ -104,7 +124,7 @@ $('article').on("click","#resolv",function(e){
 	        			'quest':quest},
 	            success: function(response){
 	                alert('thanks');
-
+	                particip();
 	            },
 	            error: function (xhr, ajaxOptions, thrownError) {
 			        alert(xhr.responseText);
@@ -118,11 +138,155 @@ $('article').on("click","#resolv",function(e){
   		var radio_value=0;
   	}
 });
-/*
-$(".quests").delegate("#resolv", "click", function(e) {
-    alert('hello3');
-});*/
+
 });
+
+function particip(){
+      $("#templates").load("templates/ev-tem-interac.html",function(){
+
+            var template = $('#interact').html();//dentro
+          
+          $('.section').html(template).trigger('create');
+
+          $('#back').attr("class-num", "2");
+      });
+
+}
+
+
+function authentic(){
+      $("#templates").load("templates/authe.html",function(){
+
+            var template = $('#authi').html();//dentro
+          
+          $('.section').html(template).trigger('create');
+
+          $('#back').attr("class-num", "2");
+      });
+      
+
+
+}
+function pregunt(){
+      $("#templates").load("templates/t-preg-hacer.html",function(){
+
+            var template = $('#hacerpreg').html();//dentro
+          
+          $('.section').html(template).trigger('create');
+
+          $('#back').attr("class-num", "2");
+      });
+      
+
+
+}
+
+//CAMBIAR LOCACION DE PREGUNTA!!!!!!!!!!!
+function preguntas(){
+    var give = $('#style-include1').html();
+    $.ajax({
+        data:
+            {
+            "que": 2,
+            //"callback":"result2",
+            },
+        //datatype: 'json',
+        type: 'GET',
+        url:"http://pietreal.herokuapp.com/interactiv/jsonquest", 
+          //"http://shielded-peak-5807.herokuapp.com/interactiv/jsonquest",//'http://localhost:8000/interactiv/jsonquest',
+        success: function(result)
+            {
+                
+            if(result){
+
+            var b = '<div>hello</div>';
+            var c = '<div>hello</div>';
+            var $a = $("#templates22");
+              $("#templates22").load("templates/t-preg-resolv.html",function(){
+                  $.each(result, function(key,val) {
+                      //alert(key);
+                      if(key ==0){
+                          //alert('hello');
+                          var template = $('#template32').html();       //internal
+                              //var template = "<h1>{{name}}</h1> ";
+                          var titulo = Mustache.to_html(template, val); //other
+                          //alert(template);
+                          $('#template32').html(titulo);
+                      }else{
+                          var template = $('#inplaces').html();           //internal
+                          var html = Mustache.to_html(template, val); //other
+                          $('#lege').append(html);                    //COLOCAR DENTRO de cuadro [32>lege]!
+                      }
+                      
+                  });
+
+                  b = $('#template32').html();            //INTERNAL, el exterior! de [lege]
+
+                  $('.quests').html(b).trigger('create');
+
+                });
+
+
+                $('#templates22').html("c");
+            $('#back').attr("class-num", "72");
+              
+            //$('.quests').page();
+            $('#style-include').html(give);
+            }
+            
+            
+        }
+    });
+
+ 
+}
+
+
+	// $('#resolv').click(function(e){
+	//   	alert('hello');
+	// 	//alert($('input[name=radio-choice]:checked').val());
+	// 	var quest=$('fieldset').attr('id');
+	// 	if ($('input[name=radio-choice]').is(':checked')){
+	//   			var rad_val = $('input:radio[name=radio-choice]:checked').val();
+
+	//   			alert(quest);
+	//   			alert(rad_val);
+	//   			$.ajax({
+	// 	            type: "GET",
+	// 	            dataType: "json",
+	// 	            url: "http://localhost:8000/interactiv/resolv",
+	// 	            data: { 'send_resul': rad_val,
+	// 	        			'quest':quest},
+	// 	            success: function(response){
+	// 	                alert('thanks');
+
+	// 	            },
+	// 	            error: function (xhr, ajaxOptions, thrownError) {
+	// 			        alert(xhr.responseText);
+	// 			        alert(thrownError);
+	// 			        alert('error');
+	// 			    }
+	// 	        });
+	// 	        //window.location.replace("evento1-interactiv-t1.html");
+	//   	}else{
+	//   		alert('not checked');
+	//   		var radio_value=0;
+	//   	}
+
+
+	// });
+	/*
+	$('#resolv').live("click",function(e){
+	  	alert('hello');
+	});
+	*/
+
+
+
+
+
+
+
 
 
 
--- assets/www/js/mustache-inclusions.js
+++ assets/www/js/mustache-inclusions.js
@@ -1,80 +1,435 @@
+var arr = new Array();
+//$(document).ready(function(){
+$(document).on("pagebeforeshow", function(e){
+//function onDeviceReady() {
+      var place_class =  $('#place').attr('class-num');
+      
+      if (place_class == 'home') {
+        //if (validarlogeado()){
+          //funcion de cargado de opciones con localstorage
+          //}else{
+          eventos_principal();
+          //}        
+      }
 
-
-function preguntas(){
-    var give = $('#style-include1').html();
-    $.ajax({
-        data:
-            {
-            "que": 2,
-            //"callback":"result2",
-            },
-        //datatype: 'json',
-        type: 'GET',
-        url:"http://pietreal.herokuapp.com/interactiv/jsonquest", 
-          //"http://shielded-peak-5807.herokuapp.com/interactiv/jsonquest",//'http://localhost:8000/interactiv/jsonquest',
-        success: function(result)
-            {
-
-            if(result){
-            var b = '<div>hello</div>';
-            var $a = $("#templates22");
-              $("#templates22").load("templates/t-preg-resolv.html",function(){
-                  $.each(result, function(key,val) {
-                      //alert(key);
-                      if(key ==0){
-                        //alert('hello');
-                        var template = $('#template32').html();
-                            //var template = "<h1>{{name}}</h1> ";
-                        var titulo = Mustache.to_html(template, val); //other
-                        //alert(template);
-                        $('#template32').html(titulo);
-                      }else{
-                      var template = $('#inplaces').html();
-                      var html = Mustache.to_html(template, val); //other
-                      $('#lege').append(html);  
-                      }
-                      
-                  });
-
-                  b = $('#template32').html();
-                  $('.quests').html(b);
-
-                  $('#style-include').html(give);
-                });
-            }
-            
-            
-        }
-    });
- 
-}
-
-$(document).ready(function(){
-
-var url="http://pietreal.herokuapp.com/json/jsonev/?callback=?"
-var old_url ="http://adnp.pythonanywhere.com/json/jsonev/?callback=?"
-$.getJSON(url, function(json) {
-    $.each(json, function(key,val) {
-
-      $("#templates").load("templates/template-event.html",function(){
-          var template = $('#template3').html();
-          var html = Mustache.to_html(template, val);
-          $('.eventus').append(html);
-      });
-    });
-
-  });
-      $(".goin").click(function(event)
+      $(document).on( "click", "#homini",function(event)
         {
-          event.preventDefault();
-          preguntas();
+
+          place_class =  $('#place').attr('class-num');
+          if (place_class == 'home'){
+
+          }else{
+            eventos_principal();
+            asignar_place('home');
+          }
           return false;
         });
 
-      $("#goin").click(function(event)
+
+
+
+//---------------------------------------------------------------------------------------------
+      // $(document).on( "click","#back",function(event)
+      //   {
+      //     //event.preventDefault();
+      //     consultas_back();
+      //       //obtener el place anterior!!!!! y colocarlo como link bajo ayuda!!!!!!!
+      //     return false;
+      //   });
+
+      $(document).on( "click","#logini",function(event)
+      //$("#goin").click(function(event)
         {
-          preguntas();
+          //alert('he');
+          //event.preventDefault();
+          //$('#place').attr('class-num','login');
+          asignar_place('login',this);//#login
+          login();
           return false;
         });
+      $(document).on( "click","#logouti",function(event)
+      //$("#goin").click(function(event)
+        {
+          //alert('he');
+          //event.preventDefault();
+          //$('#place').attr('class-num','login');
+          asignar_place('login',this);//#login
+          logout();
+          return false;
+        });
+        $(document).on( "click","#myoptions",function(event)
+        {
+          asignar_place('logeado',this);//#options
+          loged();
+          return false;
+        });
+
+
+        //----------------------------------------------------------------------------
+        $(document).on( "click","#misev",function(event)
+        {
+          asignar_place('misevents',this);//#misev
+          miseventos();
+          return false;
+        });
+
+        //proximos
+
+        $(document).on( "click","#proxev",function(event)
+        {
+          asignar_place('proxev',this);//#proximev
+          proximos_eventos();
+          return false;
+        });
+        //----------------------------------------------------------------------------
+        $(document).on( "click",".idevento",function(event)
+        {
+          //ojo!!!al inicio, cargar data hacia aca!!!
+          asignar_place('event',this);//#evento1
+          evento(); 
+          return false;
+        });
+
+        $(document).on( "click","#temas",function(event)
+        {
+          asignar_place('temas',this);//#ev-temas
+          temas();
+          return false;
+        });
+
+        //expositor
+        //horario
+
+
+
+
+        //------------------------
+        $(document).on( "click",".idtema",function(event)
+        {
+          asignar_place('tema',this);//#ev-tema1
+          tema(); 
+          return false;
+        });
+
+    
+
+    // $(document).ajaxSend(function(event, xhr, settings) {
+
+    //         xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+    // });
+
+
+
 
 });
+
+
+
+//asignar la llegada a un punto.
+function asignar_place(data,selector){
+  $('#place').attr('class-num', data);
+  location.hash = $(selector).attr('href'); 
+  arr.push(data);
+  //alert('asignado');
+}
+
+//obtener el place anterior!!!!! y colocarlo como link bajo ayuda!!!!!!!
+
+//puede ser: añadir a una lista(modificar html copiando el link q se hizo click)
+//luego cargar la funcion segun el link!! jeje!... opcional(agregar 2 o mas antiguos)
+//y atras d atras??? O.o???
+function colocar_ant_place(){
+  //ant_place =$('#place').attr('class-num');
+  
+  location.hash = $(selector).attr('href'); 
+  //alert('asignado');
+}
+
+
+
+
+
+
+
+
+
+function consultas_back(){
+ //  var back_class =  $('#back').attr('class-num');
+ //  //alert(back_class);
+ //  //switch(back_class)
+ //  //data arry
+ //  longitud= arr.length-1
+ //  alert(longitud);
+ //  alert(arr[longitud]);
+ // switch(arr[longitud])
+ //  {
+ //  case 'login'://#login
+ //        //funcion();
+ //        login();
+ //        break;
+ //  case 'logeado'://#options
+ //      //funcion();
+ //      loged();
+ //      break;
+ //  case 'misevents'://#misev
+ //      //funcion();
+ //      miseventos();
+ //      break;
+ //  case 'proxev'://#proximev
+ //      //funcion();
+ //      proximos_eventos();
+ //      break;
+ //  case 'event'://#evento1
+ //      //funcion();
+ //      evento(); 
+ //      break;
+ //  case 'temas'://#ev-temas
+ //      //funcion();
+ //      temas();
+ //      break;
+ //  case 'tema'://#ev-tema1
+ //      //funcion();
+ //      tema(); 
+ //      break;
+
+ //  }
+
+
+  // switch(back_class)
+  // {
+  // case '1':
+  //   eventos_principal();
+  //   break;
+  // case '72':
+  //   preguntas();
+
+  //   break;
+  // default:
+    //code to be executed if n is different from case 1 and 2
+  //}
+  //arr.pop();
+}
+
+
+
+function tema(){
+      $("#templates").load("templates/ev-tema.html",function(){
+
+            var template = $('#tema').html();//dentro
+          
+          $('.section').html(template).trigger('create');
+
+          $('#back').attr("class-num", "2");
+      });
+      
+
+
+}
+
+function temas(){
+      $("#templates").load("templates/ev-temas.html",function(){
+
+            var template = $('#alltemas').html();//dentro
+          
+          $('.section').html(template).trigger('create');
+
+          $('#back').attr("class-num", "2");
+      });
+   
+}
+
+
+function evento(){
+      $("#templates").load("templates/evento1.html",function(){
+
+            var template = $('#descevento').html();//dentro
+          
+          $('.section').html(template).trigger('create');
+
+          $('#back').attr("class-num", "2");
+      });
+      
+
+
+}
+
+
+function miseventos(){
+      var idusuario = window.localStorage.getItem("userid");
+      $.ajax({
+              type: "GET",//"POST",
+              dataType: "json",
+              url:"http://localhost:8000/user/events/", 
+              //"http://shielded-peak-5807.herokuapp.com/interactiv/question",//"http://localhost:8000/interactiv/question",
+              data: { 'userid': idusuario},
+              success: function(resulta){
+                  //resuc = JSON.stringify(resulta, null, 4);//jquery json dump
+                  //a = $.parseJSON(resuc);
+                  //alert(resuc);
+                  if(resulta){
+                      //$("#templates").load("templates/misEventos.html",function(){
+
+                          $("#templates").load("templates/template-event.html",function(){
+                              $.each(resulta, function(key,val) {
+                                var template = $('#template3').html();
+                                var html = Mustache.to_html(template, val);
+                                $('#template33').append(html);
+                              });
+                              a=$('#containerr').html();
+                              
+                              $('.section').html(a).trigger('create');
+                          });
+                    $('#back').attr("class-num", "72");
+                    validarlogeado();  
+                    }
+                    
+              },
+              error: function (xhr, ajaxOptions, thrownError) {
+              alert(xhr.responseText);
+              alert(thrownError);
+              alert('error');
+          }
+          });
+      // $("#templates").load("templates/misEventos.html",function(){
+
+      //       var template = $('#eventContent').html();//dentro
+          
+      //     $('.section').html(template).trigger('create');
+
+      //     $('#back').attr("class-num", "2");
+      // });
+      
+
+
+}
+
+function logout(){
+  window.localStorage.removeItem("userid");
+  validarlogeado();
+}
+function loged(){
+      //obtener userid y pw:
+      var uid = $('input[name=txtuser]').val();
+      var pass = $('input[name=txtpassword]').val();
+
+      $.ajax({
+              type: "GET",//"POST",
+              dataType: "json",
+              url:"http://localhost:8000/user/login/", 
+              //"http://shielded-peak-5807.herokuapp.com/interactiv/question",//"http://localhost:8000/interactiv/question",
+              data: { 'username': uid,
+                'password': pass},
+              success: function(resulta){
+                  //resuc = JSON.stringify(resulta, null, 4);//jquery json dump
+                  //a = $.parseJSON(resuc);
+                  //alert(resuc);
+                  
+                  window.localStorage.setItem("userid", resulta.userid);
+                  if(resulta){
+                      $("#templates").load("templates/logedprinc.html",function(){
+                        var html ="";
+                            var template = $('#logmain').html();
+                            html = Mustache.to_html(template, resulta); //other
+                            $('#logmain').html(html);
+                            b = $('#logstart').html();
+                            t = $('#tprincipal').text();
+                            $('.section').html(b).trigger('create');
+                            $('#titulopag').text(t).trigger('create');
+
+                        });
+                    $('#back').attr("class-num", "72");
+                    validarlogeado();  
+                    }
+                    
+              },
+              error: function (xhr, ajaxOptions, thrownError) {
+              alert(xhr.responseText);
+              alert(thrownError);
+              alert('error');
+          }
+          });
+      $('input[name=txtuser]').val() = "";
+      $('input[name=txtpassword]').val() = "";
+      return false;
+}
+
+
+function login(){
+     $("#templates").load("templates/login.html",function(){
+
+          var template = $('#login').html();/// internal
+        
+        $('.section').html(template).trigger('create');
+
+        $('#back').attr("class-num", "2");
+     });
+}
+
+
+
+function eventos_principal(){
+  var url="http://pietreal.herokuapp.com/json/jsonev/?callback=?" //url de proximos!!
+  var old_url ="http://adnp.pythonanywhere.com/json/jsonev/?callback=?"
+  var local_url="http://localhost:8000/json/jsonev/"
+
+  //verificacion cookie!
+  $.getJSON(local_url, function(json) {
+      $("#templates").load("templates/template-event.html",function(){
+          $.each(json, function(key,val) {
+            var template = $('#template3').html();
+            var html = Mustache.to_html(template, val);
+            $('#template33').append(html);
+          });
+          a=$('#containerr').html();
+          
+          $('.section').html(a).trigger('create');
+      });
+      
+
+    });
+}
+
+
+function proximos_eventos(){
+  var url="http://pietreal.herokuapp.com/json/jsonev/?callback=?"//url de proximos!!
+      //dar id, nombre y descripcion!!!
+  $.getJSON(url, function(json) {
+      $("#templates").load("templates/template-event.html",function(){
+          $.each(json, function(key,val) {
+            var template = $('#template3').html();
+            var html = Mustache.to_html(template, val);
+            $('#template33').append(html);
+          });
+          a=$('#containerr').html();
+          
+          $('.section').html(a).trigger('create');
+      });
+      
+
+    });
+}
+
+
+
+function validarlogeado(){
+  var idusuario = window.localStorage.getItem("userid");
+  if (idusuario != null) {
+    $('#logini').css("display", "none");
+    $('#logouti').css("display", "block");
+    return True;
+  }else{
+    $('#logini').css("display", "block");
+    $('#logouti').css("display", "none");
+    return False;
+  }
+
+}
+
+
+
+//OJO!!!! cada punto al estar logeado
+//funcion para validar existencia de 
+//o solo cambiar en el slide izquierdo q es estatico XD.
+
+
+
